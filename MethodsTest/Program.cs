f731b033843aeb87babf0b1c71bbf14a4a6cdb17
GamedevBranislav <61313508+GamedevBranislav@users.noreply.github.com>
2023-05-02 20:49:00 +0100

    Add project files.

---
+++ MethodsTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Give me an int");
+            int number = int.Parse(Console.ReadLine());
+            MultiplicationTable(number);
+        }
+
+
+        public static void MultiplicationTable(int number)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                int answer = number * i;
+                Console.WriteLine("{0} * {1} = {2}", i,number,answer);
+
+            }
+        }
+        
+    }
+}
